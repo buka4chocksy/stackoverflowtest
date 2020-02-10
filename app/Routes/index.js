ed632670d92bc9c619b99203d425af4ada9cbb43
buka4chocksy <buka4chocksy@gmail.com>
2020-02-10 22:40:45 +0100

    added question cunstom route to the index route

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -1,7 +1,8 @@
 var AuthRoutes = require('./userAuthRoutes');
-
+var questionRoutes = require('./questionRoutes')
 module.exports = function(router){
     router.use('/auth',AuthRoutes())
+    router.use('/question', questionRoutes())
 
  return router;
 }
