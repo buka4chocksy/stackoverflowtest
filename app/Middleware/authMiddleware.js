63ff6df77e9d7b4d871a7458743538ff84368fcd
buka4chocksy <buka4chocksy@gmail.com>
2020-02-10 22:39:15 +0100

    created authentication middleware

---
+++ app/Middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+var authService = require('../Service/authService');
+var User = require('../Model/auth');
+
+exports.authenticate = function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        authService.verifyToken(token).then(decoded => {
+            User.findOne({ publicId: decoded.publicId }, '').then(data => {
+                if (data == null) {
+                    res.status(401).send({ success: false, message: "User does not exist" });
+                } else {
+                    req.auth = {
+                        publicId: data.publicId,
+                        email: decoded.email,
+                        userName: data.userName,
+                        Id: data._id
+                    }
+                    res.locals.response = { data: decoded, message: "", success: true };
+                    next();
+                }
+            })
+        }).catch(err => {
+            res.status(401).send({ success: false, message: "Invalid token", data: err });
+
+        })
+    } else {
+        res.status(401).send({ success: false, message: "No token provided" });
+    }
+}
