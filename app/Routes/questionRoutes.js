0b0cfb16f1225396de9a1bb6444393276450c4a4
buka4chocksy <buka4chocksy@gmail.com>
2020-02-11 12:03:54 +0100

    added search , voterand downvote routes

--- app/Routes/questionRoutes.js
+++ app/Routes/questionRoutes.js
@@ -5,7 +5,10 @@ module.exports = function(){
     const questionCtrl = new questionController();
     router.post('/ask', middleWare.authenticate ,  questionCtrl.askQuestion);
     router.get('/:pagesize/:pagenumber', middleWare.authenticate ,  questionCtrl.getAllQuestions);
+    router.post('/search',  questionCtrl.search);
     router.post('/:id', middleWare.authenticate ,  questionCtrl.AnswerQuestions);
+    router.post('/vote/:id', middleWare.authenticate ,  questionCtrl.voteQuestion);
+    router.post('/downvote/:id', middleWare.authenticate ,  questionCtrl.downVoteQuestion);
 
     return router;
 }
