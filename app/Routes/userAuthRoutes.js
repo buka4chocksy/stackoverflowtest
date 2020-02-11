caa2a107208075c304f6ca5c55e12a537ad2c66f
buka4chocksy <buka4chocksy@gmail.com>
2020-02-11 17:04:39 +0100

    added search user end point

--- app/Routes/userAuthRoutes.js
+++ app/Routes/userAuthRoutes.js
@@ -4,6 +4,6 @@ module.exports = function(){
     const authCtrl = new authController();
     router.post('/register', authCtrl.register);
     router.post('/authenticate', authCtrl.login);
-
+    router.get('/search', authCtrl.search);
     return router;
 }
